ce72a5e2ebb159540d914364a4b9b145957f3322
lashalego <150528967+lashalego@users.noreply.github.com>
2024-05-29 18:42:52 +0400

    Add files via upload

---
+++ Js/Ethno.js
@@ -0,0 +1,22 @@
+$('.dropdown-toggle').click(function () {
+    
+    let menu = $('.dropdown-menu');
+    menu.toggleClass('show');
+} );
+$('.lang-toggle').click(function () {
+    
+    let menu = $('.lang-menu');
+    menu.toggleClass('show');
+} );
+$('.left-opener').click(function () {
+    let hide = $('.left-menu');
+    hide.addClass('show-menu');
+    let open = $('.left-opener');
+    open.addClass('hide')
+} );
+$('.close-hide').click(function () {
+    let hide = $('.left-menu');
+    hide.removeClass('show-menu');
+    let open = $('.left-opener');
+    open.removeClass('hide')
+});
